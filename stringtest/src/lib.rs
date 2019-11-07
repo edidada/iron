fec0f7e00181308a8c45a56690fee227c00b9e7d
edidada <sandisks555@gmail.com>
2019-11-07 19:39:13 +0800

    add string test

---
+++ stringtest/src/lib.rs
@@ -0,0 +1,33 @@
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    fn string_test() {
+        let mut s = String::new();
+
+
+        let data = "initial contents";
+
+        let s = data.to_string();
+
+// 该方法也可直接用于字符串字面值：
+        let s = "initial contents".to_string();
+
+        let s = String::from("initial contents");
+    }
+
+    #[test]
+    fn string_varibale(){
+        let mut s = String::from("foo");
+        s.push_str("bar");
+
+        let mut s1 = String::from("foo");
+        let s2 = "bar";
+        s1.push_str(s2);
+        println!("s2 is {}", s2);
+    }
+}
